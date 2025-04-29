4b1ccd3df7138b362634aacca9783f9ca1259a46
Zvonimir Sabljic <zvonimir@pythagora.io>
2025-04-28 20:00:02 -0700

    Fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // Import design system styles
-import '../../design/dist/main.css'
+import '../design/dist/main.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
