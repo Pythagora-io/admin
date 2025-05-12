975e5cdb6746053ad6530c154ee94295968da993
Elijah <limaj.sulejman@gmail.com>
2025-05-12 11:41:20 +0200

    Add background image code from forked repo to original repo

--- client/src/components/BackgroundImage.tsx
+++ client/src/components/BackgroundImage.tsx
@@ -1,33 +1,24 @@
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import bleed from "@/assets/bleed.png"
 
 interface BackgroundImageProps {
-  className?: string;
+  className?: string
 }
 
 export function BackgroundImage({ className }: BackgroundImageProps) {
   return (
-    <>
-      {/* Full background image with blur */}
-      <div
-        className={cn(
-          "fixed inset-0 w-[95%] h-[95%] m-auto -z-10 bg-cover bg-center rounded-xl",
-          className,
-        )}
-        style={{
-          backgroundImage: "url('/images/abstract-bg.jpg')",
-          filter: "blur(8px)",
-          opacity: 0.15,
-        }}
+    <div
+      className={cn(
+        // offset by sidebar width (20rem), span to right
+        "fixed bottom-0 left-80 right-0 h-20 overflow-hidden pointer-events-none -z-10",
+        className
+      )}
+    >
+      <img
+        src={bleed}
+        alt=""
+        className="absolute bottom--20 left-0 w-full h-auto"
       />
-
-      {/* Non-blurred border area (5% on each side) */}
-      <div
-        className="fixed inset-0 -z-20 bg-cover bg-center"
-        style={{
-          backgroundImage: "url('/images/abstract-bg.jpg')",
-          opacity: 0.1,
-        }}
-      />
-    </>
-  );
+    </div>
+  )
 }
